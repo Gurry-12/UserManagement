aea245888821a915fe9308f1b9b6d2a697edf3b9
GURPREET SINGH <singhsarpreet234@gmail.com>
2025-06-10 17:14:43 +0530

    done

---
+++ UserManagement.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagement.Application.Services.AbstractServices;
+
+namespace UserManagement.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _user;
+
+        public UserController(IUserService user)
+        {
+            _user = user;
+        }
+
+        [HttpGet("GetAllUsers")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var user = await _user.GetAllUsers();
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
